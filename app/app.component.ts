252516a5e6e8e4bedafb10b5e5b92cec79393a1a
viktor <viktortat@gmail.com>
2016-03-24 18:06:06 +0200

    1

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,6 +15,16 @@ import {HeroesComponent} from "./heroes.component";
     </nav>
     
     <router-outlet></router-outlet>
+    
+    <div [ngClass]="['bold-text', 'green']">array of classes</div>
+    <div [ngClass]="'italic-text blue'">string of classes</div>
+    <div [ngClass]="{'small-text': true, 'red': true}">object of classes</div>
+    <div [ngStyle]="{'color': color, 'font-size': size, 'font-weight': 'bold'}">style using ngStyle</div>
+    <div [style.color]="'orange'">style using property syntax, this text is orange</div>
+    
+    <input [(ngModel)]="color" />
+    <button (click)="size = size + 1">+</button>
+    <button (click)="size = size - 1">-</button>
     `,
     directives: [ROUTER_DIRECTIVES],
     providers: [HeroService]
